7611cbaedf1c3e5798e1112fe71b691709281347
mzmacaulaydb <59340825+mzmacaulaydb@users.noreply.github.com>
2020-01-02 15:02:36 +0000

    Update Branches10.sql

--- Branches10.sql
+++ Branches10.sql
@@ -1,3 +1,17 @@
+-- All
+-- Branches (10)
+-- Staff (100)
+-- Products (100)
+-- Stock (1,000)
+-- Customer (1,000)
+-- Orders (1,000)
+-- Prices (1,000)
+
+SET foreign_key_checks = 0; -- switch back on at end of file
+
+
+
+
 insert into Branches (BID, BNAME, BADD, BMAN) values (10, 'Ash Shāmīyah', '82684 Randy Park', 6);
 insert into Branches (BID, BNAME, BADD, BMAN) values (1, 'Tampocon', '3 Stephen Trail', 10);
 insert into Branches (BID, BNAME, BADD, BMAN) values (9, 'Chaem Luang', '20617 Schmedeman Court', 6);
@@ -8,3 +22,7 @@ insert into Branches (BID, BNAME, BADD, BMAN) values (2, 'Moyamba', '8 Hooker Pl
 insert into Branches (BID, BNAME, BADD, BMAN) values (1, 'Cermee', '45 Moose Terrace', 8);
 insert into Branches (BID, BNAME, BADD, BMAN) values (10, 'Smyshlyayevka', '491 Columbus Way', 2);
 insert into Branches (BID, BNAME, BADD, BMAN) values (1, 'Turets-Bayary', '05986 Mariners Cove Alley', 10);
+
+
+
+SET foreign_key_checks = 1;
